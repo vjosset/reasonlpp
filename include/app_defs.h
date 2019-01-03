af04f304601e16f9976bd8e3199acb02c0d7ba1e
VJ <vjosset@gmail.com>
2019-01-02 20:50:40 -0500

    Initial commit

---
+++ include/app_defs.h
@@ -0,0 +1,244 @@
+
+#ifndef APP_TYPES_H
+#define APP_TYPES_H
+
+/******************************************************************************
+ 
+ Copyright (c) 2015, Focusrite Audio Engineering Ltd.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ * Neither the name of Focusrite Audio Engineering Ltd., nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ *****************************************************************************/
+
+// ____________________________________________________________________________
+//
+// Don't modify this file!  This declares the binary interface to the library,
+// so modifying it will probably break things.
+// ____________________________________________________________________________
+//
+// Types
+// ____________________________________________________________________________
+
+typedef signed long  s32;
+typedef signed short s16;
+typedef signed char  s8;
+
+typedef unsigned long  u32;
+typedef unsigned short u16;
+typedef unsigned char  u8;
+
+// ____________________________________________________________________________
+//
+// App structure
+// ____________________________________________________________________________
+
+#define	TYPEPAD				0
+#define	TYPESETUP			1
+
+#define	MAXLED				63
+
+// ____________________________________________________________________________
+//
+// Useful MIDI constants
+// ____________________________________________________________________________
+
+#define	NOTEON				0x90
+#define	NOTEOFF				0x80
+#define	POLYAFTERTOUCH		0xA0
+#define	CC					0xB0
+#define	CHANNELAFTERTOUCH   0xD0
+#define	SONGPOSITIONPOINTER 0xF2
+#define	MIDITIMINGCLOCK     0xF8
+#define	MIDISTART           0xFA
+#define	MIDICONTINUE        0xFB
+#define	MIDISTOP            0xFC
+
+// ____________________________________________________________________________
+//
+// MIDI ports
+// ____________________________________________________________________________
+
+// USB MIDI: "Standalone" port
+#define	USBSTANDALONE		0
+
+// USB MIDI: "MIDI" port
+#define	USBMIDI				1
+
+// MIDI DIN port
+#define	DINMIDI				2
+
+// ____________________________________________________________________________
+//
+// MIDI Jack Socket Switch IDs
+// ____________________________________________________________________________
+
+#define	MIDI_IN_CABLE		0
+#define	MIDI_OUT_CABLE		1
+
+// ____________________________________________________________________________
+//
+// Flash storage
+// ____________________________________________________________________________
+
+#define USER_AREA_SIZE 1024
+
+// ____________________________________________________________________________
+//
+// Raw ADC reads. For technical reasons, the ADC layout is not the same as the
+// LED layout. Be sure to translate ADC values using the table below as you
+// need them!
+// ____________________________________________________________________________
+
+#define PAD_COUNT 64
+
+static const u8 ADC_MAP[PAD_COUNT] =
+{
+	11, 51, 12, 52, 13, 53, 14, 54,
+	15, 55, 16, 56, 17 ,57, 18, 58,
+	21, 61, 22, 62, 23, 63, 24, 64,
+	25, 65, 26, 66, 27, 67, 28, 68,
+	31, 71, 32, 72, 33, 73, 34, 74,
+	35, 75, 36, 76, 37, 77, 38, 78,
+	41, 81, 42, 82, 43, 83, 44, 84,
+	45, 85, 46, 86, 47, 87, 48, 88,
+};
+
+// ____________________________________________________________________________
+
+
+// Button shortcuts
+#define TOP1 		91
+#define TOP2 		92
+#define TOP3 		93
+#define TOP4 		94
+#define TOP5 		95
+#define TOP6 		96
+#define TOP7 		97
+#define TOP8 		98
+
+#define LEFT1 		10
+#define LEFT2 		20
+#define LEFT3 		30
+#define LEFT4 		40
+#define LEFT5 		50
+#define LEFT6 		60
+#define LEFT7 		70
+#define LEFT8 		80
+
+#define RIGHT1		19
+#define RIGHT2		29
+#define RIGHT3		39
+#define RIGHT4		49
+#define RIGHT5		59
+#define RIGHT6		69
+#define RIGHT7		79
+#define RIGHT8		89
+
+#define BOTTOM1		 1
+#define BOTTOM2		 2
+#define BOTTOM3		 3
+#define BOTTOM4		 4
+#define BOTTOM5		 5
+#define BOTTOM6		 6
+#define BOTTOM7		 7
+#define BOTTOM8		 8
+
+#define PAD11		11
+#define PAD12		12
+#define PAD13		13
+#define PAD14		14
+#define PAD15		15
+#define PAD16		16
+#define PAD17		17
+#define PAD18		18
+
+#define PAD21		21
+#define PAD22		22
+#define PAD23		23
+#define PAD24		24
+#define PAD25		25
+#define PAD26		26
+#define PAD27		27
+#define PAD28		28
+
+#define PAD31		31
+#define PAD32		32
+#define PAD33		33
+#define PAD34		34
+#define PAD35		35
+#define PAD36		36
+#define PAD37		37
+#define PAD38		38
+
+#define PAD41		41
+#define PAD42		42
+#define PAD43		43
+#define PAD44		44
+#define PAD45		45
+#define PAD46		46
+#define PAD47		47
+#define PAD48		48
+
+#define PAD51		51
+#define PAD52		52
+#define PAD53		53
+#define PAD54		54
+#define PAD55		55
+#define PAD56		56
+#define PAD57		57
+#define PAD58		58
+
+#define PAD61		61
+#define PAD62		62
+#define PAD63		63
+#define PAD64		64
+#define PAD65		65
+#define PAD66		66
+#define PAD67		67
+#define PAD68		68
+
+#define PAD71		71
+#define PAD72		72
+#define PAD73		73
+#define PAD74		74
+#define PAD75		75
+#define PAD76		76
+#define PAD77		77
+#define PAD78		78
+
+#define PAD81		81
+#define PAD82		82
+#define PAD83		83
+#define PAD84		84
+#define PAD85		85
+#define PAD86		86
+#define PAD87		87
+#define PAD88		88
+
+#define BUTTON_COUNT 100
+
+#endif
---
+++ src/app.c
@@ -0,0 +1,493 @@
+/******************************************************************************
+ 
+ Copyright (c) 2015, Focusrite Audio Engineering Ltd.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ * Neither the name of Focusrite Audio Engineering Ltd., nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ *****************************************************************************/
+
+//______________________________________________________________________________
+//
+// Headers
+//______________________________________________________________________________
+
+#include "app.h"
+
+/*
+Map:
+	
+		+---+---+---+---+---+---+---+---+
+	    | 91| 92| 93| 94| 95| 96| 97| 98|
+	+---+---+---+---+---+---+---+---+---+---+
+	| 80| 81| 82| 83| 84| 85| 86| 87| 88| 89+
+	+---+---+---+---+---+---+---+---+---+---+
+	| 70| 71| 72| 73| 74| 75| 76| 77| 78| 79+
+	+---+---+---+---+---+---+---+---+---+---+
+	| 60| 61| 62| 63| 64| 65| 66| 67| 68| 69+
+	+---+---+---+---+---+---+---+---+---+---+
+	| 50| 51| 52| 53| 54| 55| 56| 57| 58| 59+
+	+---+---+---+---+---+---+---+---+---+---+
+	| 40| 42| 42| 43| 44| 45| 46| 47| 48| 49+
+	+---+---+---+---+---+---+---+---+---+---+
+	| 30| 31| 32| 33| 34| 35| 36| 37| 38| 39+
+	+---+---+---+---+---+---+---+---+---+---+
+	| 20| 21| 22| 23| 24| 25| 26| 27| 28| 29+
+	+---+---+---+---+---+---+---+---+---+---+
+	| 10| 11| 12| 13| 14| 15| 16| 17| 18| 19+
+	+---+---+---+---+---+---+---+---+---+---+
+	    | 01| 02| 03| 04| 05| 06| 07| 08|
+	    +---+---+---+---+---+---+---+---+
+
+
+	    +---+---+---+---+---+---+---+---+
+	    | UP|DWN|LFT|RGT|   |   |   |   |
+	+---+---+---+---+---+---+---+---+---+---+
+	|   | E3| F3|F#3| G3| F1| F2| F3| F4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	|   | C3|C#3| D3|D#3| F1| F2| F3| F4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	|   |G#2| A2|A#2| B2| F1| F2| F3| F4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	|   | E2| F2|F#2| G2| F1| F2| F3| F4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	|   | C2|C#2| D2|D#2| F1| F2| F3| F4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	|   |G#1| A1|A#1| B1| F1| F2| F3| F4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	|   | E1| F1|F#1| G1| F1| F2| F3| F4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	|VEL| C1|C#1| D1|D#1| T1| T2| T3| T4|   +
+	+---+---+---+---+---+---+---+---+---+---+
+	    |   |   |   |   |   |STP|PLY|REC|
+	    +---+---+---+---+---+---+---+---+
+
+*/
+
+// Define bool type
+typedef int bool;
+#define true 1
+#define false 0
+
+// Current octave
+static u8 current_octave = 1;
+
+//Full velocity toggle
+bool full_velocity = false;
+
+//Button shortcuts
+#define BTN_OCTAVEUP 		TOP1
+#define BTN_OCTAVEDOWN 		TOP2
+#define BTN_TOGGLEFULLVEL 	LEFT1
+#define BTN_PREVINST		TOP3
+#define BTN_NEXTINST		TOP4
+#define BTN_PLAY			BOTTOM6
+#define BTN_STOP			BOTTOM7
+#define BTN_REC				BOTTOM8
+
+//Color constants
+static u8 color_off[3] 		= { 0,  0,  0};
+static u8 color_wht[3] 		= {63, 63, 63};
+
+static u8 color_red[3] 		= {63,  0,  0};
+static u8 color_red_dark[3]	= { 4,  0,  0};
+static u8 color_grn[3] 		= { 0, 63,  0};
+static u8 color_blu[3] 		= { 0,  0, 63};
+
+static u8 color_ylw[3] 		= {63, 63,  0};
+static u8 color_org[3] 		= {63, 32,  0};
+static u8 color_dor[3] 		= {63, 16,  0};
+
+//Color for button presses
+static u8 color_pressed[3]	= {63, 63, 63};
+
+//Colors for toggles
+static u8 color_toggle_on[3] = {0, 63, 0};
+static u8 color_toggle_off[3] = {0, 4, 0};
+
+//Colors for faders
+static u8 color_fader_on[3] = {0, 0, 63};
+static u8 color_fader_off[3] = {0, 0, 4};
+
+//Default map of pad colors (init and returning to default state)
+static u8 default_color_map[BUTTON_COUNT][3] = 
+{  
+	{ 0,  0,  0}, {63,  0,  0}, {63,  0,  0}, { 0,  0,  0}, { 0,  0,  0}, { 0,  0,  0}, {63,  0,  0}, {63,  0,  0}, {63,  0,  0}, { 0,  0,  0},
+	{ 4,  0,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, { 0,  4,  0}, { 0,  4,  0}, { 0,  4,  0}, { 0,  4,  0}, { 0,  0,  0},
+	{ 0,  0,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0},
+	{ 0,  0,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63,  0,  0}, {63,  0,  0}, { 0,  0,  0}, { 0,  0,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}
+};
+
+//Current map of pad colors
+static u8 color_map[BUTTON_COUNT][3] = 
+{  
+	{ 0,  0,  0}, {63,  0,  0}, {63,  0,  0}, { 0,  0,  0}, { 0,  0,  0}, { 0,  0,  0}, {63,  0,  0}, {63,  0,  0}, {63,  0,  0}, { 0,  0,  0},
+	{ 4,  0,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, { 0,  4,  0}, { 0,  4,  0}, { 0,  4,  0}, { 0,  4,  0}, { 0,  0,  0},
+	{ 0,  0,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, {63, 63,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0},
+	{ 0,  0,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, {63, 32,  0}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, {63, 16,  0}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  4}, { 0,  0,  0}, 
+	{ 0,  0,  0}, {63,  0,  0}, {63,  0,  0}, { 0,  0,  0}, { 0,  0,  0}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0, 63}, { 0,  0,  0}
+};
+
+static const u8 note_map[BUTTON_COUNT] =
+{
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0, 24, 25, 26, 27,  0,  0,  0,  0,  0, 
+	 0, 28, 29, 30, 31,  0,  0,  0,  0,  0, 
+	 0, 32, 33, 34, 35,  0,  0,  0,  0,  0, 
+	 0, 36, 37, 38, 39,  0,  0,  0,  0,  0, 
+	 0, 40, 41, 42, 43,  0,  0,  0,  0,  0, 
+	 0, 44, 45, 46, 47,  0,  0,  0,  0,  0, 
+	 0, 48, 49, 50, 51,  0,  0,  0,  0,  0, 
+	 0, 52, 53, 54, 55,  0,  0,  0,  0,  0,
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+};
+
+//Maps x-index of button to CC number to send
+static const u8 fader_cc_map[10] = { 0, 0, 0, 0, 0, 71, 72, 73, 74, 0 };
+
+//Maps y-index of button to CC value to send
+static const u8 fader_value_map[10] = { 0, 0, 0, 21, 42, 63, 84, 105, 127, 127 };
+
+//Holds values of the toggles
+static const u8 toggle_cc_map[BUTTON_COUNT] =
+{
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0, 75, 76, 77, 78,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+};
+
+
+//Holds values of the toggles
+static u8 toggle_value_map[BUTTON_COUNT] =
+{
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+};
+
+//-----------------------------------------------------------------------------------------
+//                                HELPER FUNCTIONS
+//-----------------------------------------------------------------------------------------
+
+/**
+ * Returns a value indicating whether the specified button value indicates a press event
+ */
+bool is_press(u8 value) {
+	return value != 0;
+}
+
+/**
+ * Returns a value indicating whether the specified button value indicates a release event
+ */
+bool is_release(u8 value) {
+	return value == 0;
+}
+
+/**
+ * Draws the current color map to the hardward layer
+ */
+void draw_color_map() {
+	for (u8 i = 0; i < BUTTON_COUNT; i++) {
+		//Send color_map[i] to the button
+		hal_plot_led(TYPEPAD, i, color_map[i][0], color_map[i][1], color_map[i][2]);
+	}
+}
+ 
+/**
+ * Returns a value indicating whether the specified button index is a pad (as opposed to a util button)
+ */
+bool is_pad(u8 index) {
+	return index > 10 && index < 89 && index % 10 != 0 && index % 10 != 9;
+}
+
+/**
+ * Returns a value indicating whether the specified button index is a note
+ */
+bool is_note(u8 index) {
+	return is_pad(index) && (index % 10) < 5;
+}
+
+/**
+ * Returns a value indicating whether the specified button index is a fader
+ */
+bool is_fader(u8 index) {
+	return is_pad(index) && (index % 10) > 4 && (index / 10) > 1;
+}
+
+/**
+ * Returns a value indicating whether the specified button index is a toggle
+ */
+bool is_toggle(u8 index) {
+	return is_pad(index) && (index % 10) > 4 && (index / 10) == 1;
+}
+
+/**
+ * Returns a value indicating whether the specified button index is a utility button
+ */
+bool is_util(u8 index) {
+	return !is_pad(index);
+}
+
+/**
+ * Sets the color on color_map for the specified index to the specified color
+ */
+void set_color_map(u8 index, u8 color[]) {
+	color_map[index][0] = color[0];
+	color_map[index][1] = color[1];
+	color_map[index][2] = color[2];
+}
+
+/**
+ * Increments the current octave value
+ */
+void octave_up() {
+	if (current_octave < 7) {
+		current_octave++;
+	}
+	
+	if (current_octave == 7) {
+		//Disable octave up button, already at max
+		set_color_map(BTN_OCTAVEUP, color_off);
+	}
+	
+	//Enable octave down button
+	set_color_map(BTN_OCTAVEDOWN, color_red);
+}
+
+/**
+ * Decrements the current octave value
+ */
+void octave_down() {
+	if (current_octave > 0) {
+		current_octave--;
+	}
+	
+	if (current_octave == 0) {
+		//Disable octave down button, already at min
+		set_color_map(BTN_OCTAVEDOWN, color_off);
+	}
+	
+	//Enable octave up button
+	set_color_map(BTN_OCTAVEUP, color_red);
+}
+
+/**
+ * Toggles full velocity mode
+ */
+void toggle_full_velocity() {
+	full_velocity = !full_velocity;
+	set_color_map(BTN_TOGGLEFULLVEL, full_velocity ? color_red : color_red_dark);
+}
+
+
+//-----------------------------------------------------------------------------------------
+//                                EVENTS
+//-----------------------------------------------------------------------------------------
+
+void event_note(u8 index, u8 value) {
+	if (is_press(value)) {
+		//Send MIDI note on (with full_velocity check)
+		hal_send_midi(USBMIDI, NOTEON | 0, note_map[index] + (12 * current_octave), full_velocity ? 127 : value);
+		
+		//Set the pad to its pressed color
+		set_color_map(index, color_pressed);
+	} else {
+		//Send MIDI note off
+		hal_send_midi(USBMIDI, NOTEON | 0, note_map[index] + (12 * current_octave), 0);
+		
+		//Reset the note pad to its default color
+		set_color_map(index, default_color_map[index]);
+	}
+}
+
+void event_toggle(u8 index, u8 value) {
+	if (is_press(value)) {
+		//Send MIDI CC value
+		u8 cc_number = toggle_cc_map[index];
+		u8 cc_value = toggle_value_map[index];
+		
+		//Now negate the value
+		if (cc_value == 127) {
+			cc_value = 0;
+		} else {
+			cc_value = 127;
+		}
+		toggle_value_map[index] = cc_value;
+		
+		//Send MIDI message
+		hal_send_midi(USBMIDI, CC | 0, cc_number, cc_value);
+		
+		//Set the color of the toggle
+		set_color_map(index, cc_value == 0 ? color_toggle_off : color_toggle_on);
+	}
+}
+
+void event_fader(u8 index, u8 value) {
+	if (is_press(value)) {
+		//Send MIDI CC value
+		u8 y_index = index / 10;
+		u8 x_index = index % 10;
+		
+		u8 cc_value = fader_value_map[y_index];
+		u8 cc_number = fader_cc_map[x_index];
+		
+		hal_send_midi(USBMIDI, CC | 0, cc_number, cc_value);
+		
+		//Current button will be lit (color_fader_on)
+		//Buttons below will be lit (color_fader_on)
+		//Buttons above will be off (color_fader_off)
+		
+		for (u8 i = index; i > 20; i -= 10) {
+			set_color_map(i, color_fader_on);
+		}
+		for (u8 i = index + 10; i < 90; i += 10) {
+			set_color_map(i, color_fader_off);
+		}
+	} else {
+		//Release - Reset color
+	}
+}
+
+void event_util(u8 index, u8 value) {
+	if (is_press(value)) {
+		switch (index) {
+			case BTN_OCTAVEUP:
+				octave_up();
+				break;
+			case BTN_OCTAVEDOWN:
+				octave_down();
+				break;
+			case BTN_TOGGLEFULLVEL:
+				toggle_full_velocity();
+				break;
+			case BTN_PREVINST:
+				break;
+			case BTN_NEXTINST:
+				break;
+			case BTN_PLAY:
+				break;
+			case BTN_STOP:
+				break;
+			case BTN_REC:
+				break;
+		}
+	}
+}
+
+/**
+ * Called when the user presses or releases any button or pad on the control surface.
+ *
+ * @param type - TYPEPAD for normal pads or buttons, TYPESETUP for the Setup button
+ * @param index - The index of the button, as detailed at the start of this file.
+ * @param value - 0 for release, nonzero for press.
+ */
+void app_surface_event(u8 type, u8 index, u8 value) {
+	if (type == TYPEPAD) {
+		//Mark this button as pressed
+		//set_color_map(index, color_pressed);
+		
+		if (is_util(index)) {
+			//This is a utility button
+			event_util(index, value);
+		} else if (is_fader(index)) {
+			//This is a fader button
+			event_fader(index, value);
+		} else if (is_note(index)) {
+			//This is a note button
+			event_note(index, value);
+		} else if (is_toggle(index)) {
+			//This is a toggle button
+			event_toggle(index, value);
+		}
+		
+		//Draw color_map
+		draw_color_map();
+	}
+}
+
+//______________________________________________________________________________
+
+void app_midi_event(u8 port, u8 status, u8 d1, u8 d2) {
+}
+
+//______________________________________________________________________________
+
+void app_sysex_event(u8 port, u8 * data, u16 count) {
+}
+
+//______________________________________________________________________________
+
+void app_aftertouch_event(u8 index, u8 value) {
+    if (is_note(index)) {
+		//This is a note pad
+		hal_send_midi(USBMIDI, POLYAFTERTOUCH | 0, index, value);
+	}
+}
+
+//______________________________________________________________________________
+
+void app_cable_event(u8 type, u8 value) {
+}
+
+//______________________________________________________________________________
+
+void app_timer_event() {
+}
+
+//______________________________________________________________________________
+
+void app_init(const u16 *adc_raw) {
+	//Set up starting octave
+	current_octave = 1;
+	
+	//Draw the default color map
+	draw_color_map();
+}
